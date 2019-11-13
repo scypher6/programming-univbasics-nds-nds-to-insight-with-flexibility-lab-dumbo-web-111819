fb98f24a84f6b0e31337c709aa81a577f2d39c63
Joseph Pyram <joseph.pyram@gmail.com>
2019-11-13 20:29:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -99,6 +99,13 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  result = []
+  
+  source.each do |movie|
+    result << movies_with_director_key(movie[:name], movie[:movies])
+  end
+  
+  result
 end
 
 # ----------------    End of Your Code Region --------------------
